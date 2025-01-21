e2c9ffd2ac55bce1a7a5839a62f2393211d0a580
Vdaas CI <vald@vdaas.org>
2025-01-21 06:24:53 +0000

    Update [vald sha: 8179e7d372fd1b9affc84649eb05fa178f8b03a0]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- src/main/java/org/vdaas/vald/api/v1/vald/RemoveGrpc.java
+++ src/main/java/org/vdaas/vald/api/v1/vald/RemoveGrpc.java
@@ -9,7 +9,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.69.0)",
+    value = "by gRPC proto compiler (version 1.69.1)",
     comments = "Source: v1/vald/remove.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class RemoveGrpc {
