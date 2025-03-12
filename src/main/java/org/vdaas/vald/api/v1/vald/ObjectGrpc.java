aac346b5028ece8daad9a4390b1e6bf701988f4c
Vdaas CI <vald@vdaas.org>
2025-03-12 01:21:37 +0000

    Update [vald sha: 29272c42df5b1775d4fc2c480ff58160a1ea762e]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- src/main/java/org/vdaas/vald/api/v1/vald/ObjectGrpc.java
+++ src/main/java/org/vdaas/vald/api/v1/vald/ObjectGrpc.java
@@ -9,7 +9,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.70.0)",
+    value = "by gRPC proto compiler (version 1.71.0)",
     comments = "Source: v1/vald/object.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class ObjectGrpc {
