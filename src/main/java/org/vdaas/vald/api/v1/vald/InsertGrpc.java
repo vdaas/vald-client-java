ff205af55da85afb03f7a3931f4d0466b17e2ad8
Vdaas CI <vald@vdaas.org>
2025-04-18 01:37:55 +0000

    Update [vald sha: aaba0834fb54eb7b6828b3a9718ad648f0f8bb93]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- src/main/java/org/vdaas/vald/api/v1/vald/InsertGrpc.java
+++ src/main/java/org/vdaas/vald/api/v1/vald/InsertGrpc.java
@@ -9,7 +9,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.71.0)",
+    value = "by gRPC proto compiler (version 1.72.0)",
     comments = "Source: v1/vald/insert.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class InsertGrpc {
